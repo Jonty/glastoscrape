1bec33de1462b0cb26c47e1b96b22574de76715c
Jonty Wareing <jonty@jonty.co.uk>
2017-05-31 11:36:29 +0100

    Raw glastonbury 2017 data

--- glastoscrape.py
+++ glastoscrape.py
@@ -2,7 +2,7 @@
 import lxml.html
 import unicodecsv
 
-site = 'http://www.glastonburyfestivals.co.uk/line-up/line-up-2016/?artist'
+site = 'http://www.glastonburyfestivals.co.uk/line-up/line-up-2017/?artist'
 root = lxml.html.parse(site).getroot()
 
 artist_nodes = root.xpath('//*[@id=\"main\"]/div[2]/div/ul/li')
@@ -18,8 +18,18 @@ for node in artist_nodes:
         title = children[0]
 
     if title.text:
-        data.append([title.text.strip(), link, stage.text.strip(), day.text.strip(), time.text.strip()])
+        title = title.text.strip()
+        stage = stage.text.strip()
+        if day.text:
+            day = day.text.strip()
+        else:
+            day = None
+        if time.text:
+            time = time.text.strip()
+        else:
+            time = None
+        data.append([title, link, stage, day, time])
 
-with open('glastonbury_2016_schedule.csv', 'w') as fp:
+with open('glastonbury_2017_schedule.csv', 'w') as fp:
     out = unicodecsv.writer(fp, delimiter=',', encoding='utf-8')
     out.writerows(data)
