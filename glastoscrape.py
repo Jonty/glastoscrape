98133d7b357eb65f8acbecb8435bb07121cd0c00
Jonty Wareing <jonty@jonty.co.uk>
2023-06-04 00:56:41 +0100

    glastonbury 2023

--- glastoscrape.py
+++ glastoscrape.py
@@ -3,16 +3,23 @@ from lxml import html
 import csv
 import requests
 
-response = requests.get('https://www.glastonburyfestivals.co.uk/line-up/line-up-2022/?artist')
+year = 2023
+
+response = requests.get('https://www.glastonburyfestivals.co.uk/line-up/line-up-%s/?artist' % year)
 root = html.document_fromstring(response.content)
 
 artist_nodes = root.xpath('//*[@id=\"main\"]/div[2]/div/ul/li')
 
-data = [['title', 'url', 'stage', 'day', 'time']]
+data = [['title', 'url', 'stage', 'day', 'time', 'description']]
 
 for node in artist_nodes:
     title, stage, day, time = node.getchildren()
     link = None
+    description = None
+
+    if 'title' in title.attrib:
+        description = title.attrib['title'].strip()
+
     children = title.getchildren()
     if children:
         link = children[0].attrib['href'].strip()
@@ -29,8 +36,8 @@ for node in artist_nodes:
             time = time.text.strip()
         else:
             time = None
-        data.append([title, link, stage, day, time])
+        data.append([title, link, stage, day, time, description])
 
-with open('glastonbury_2022_schedule.csv', 'w') as fp:
+with open('glastonbury_%s_schedule.csv' % year, 'w') as fp:
     out = csv.writer(fp, delimiter=',')
     out.writerows(data)
